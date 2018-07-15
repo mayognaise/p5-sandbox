91ac0e41ff407370501ee644f1f2f7871d73f29f
mayognaise <tobimayo@gmail.com>
2018-07-14 17:37:22 -0700

    dist

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin()
+    ]
+  }
 })
