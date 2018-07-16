0847401cc9135573cc966ab2974aa57be8a9b203
mayognaise <tobimayo@gmail.com>
2018-07-15 19:11:55 -0700

    adjustment

--- code/coding-train/cc2-menger-sponge-fractal/sketch.js
+++ code/coding-train/cc2-menger-sponge-fractal/sketch.js
@@ -4,7 +4,7 @@ let stats
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL)
-  const size = Math.min(windowWidth, windowHeight) / 3
+  const size = Math.min(windowWidth, windowHeight) / 2
   sponge.push(new Box(0, 0, 0, size))
   // for debug
   stats = new Stats()
@@ -14,8 +14,8 @@ function setup() {
 
 function draw() {
   background(0)
-  ambientLight(50);
-  directionalLight(255, 0, 0, 0.25, 0.25, 0)
+  ambientLight(100);
+  directionalLight(255, 255, 255, 0.25, 0.25, 0)
   const angleY = map(mouseX, 0, width, PI, 0)
   const angleX = map(mouseY, 0, height, PI, 0)
   rotateX(angleX)
