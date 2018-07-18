0a7d4aaaecb28d86560ad39329c5bb2225a73b28
mayognaise <tobimayo@gmail.com>
2018-07-18 03:21:49 -0700

    bug

--- scripts/build-md/build.js
+++ scripts/build-md/build.js
@@ -198,7 +198,7 @@ const renderMainPage = () => {
 
 /* ======================================
  * render
- ====================================== */`
+ ====================================== */
 renderMainPage()
 return
 const projedtKeys = Object.keys(data.projects)
